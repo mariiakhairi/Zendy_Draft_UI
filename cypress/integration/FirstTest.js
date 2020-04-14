fa43943618763c0ce83b0e0a5921bdb9f325ab48
mariiakhairi <mariia_khairi@inbox.ru>
2020-04-14 12:41:22 +0400

    new test added

--- cypress/integration/FirstTest.js
+++ cypress/integration/FirstTest.js
@@ -23,7 +23,7 @@ describe('App Home Page', () => {
     cy.get('.css-yvxfk9-search-result-root').find('[data-test="search-result-0"]').should('have.length', 1);
   })
 
-  it.only('Login, search for test open the first card', () => {
+  it('Login, search for test open the first card', () => {
     cy.request('POST', 'https://api.dev.zendy.io/auth/auth', { "phoneNumber": "+971122222222", "password": "Password1234" })
     .then((response) => {
       expect(response.status).to.eq(200)
@@ -33,6 +33,20 @@ describe('App Home Page', () => {
     cy.get('.css-58mwrr-rowContainer > .MuiButtonBaseroot-0-1-7').click();
     cy.wait(2000);
     cy.get('.css-yvxfk9-search-result-root').find('[data-test="search-result-0"]').click();
+    cy.get('[data-test="search-result-0"]').contains('Read').click();
+
+  })
+
+  it('Login, go to search page, search test, aplly peer Reviewed filter, check results ', () => {
+    cy.request('POST', 'https://api.dev.zendy.io/auth/auth', { "phoneNumber": "+971122222222", "password": "Password1234" })
+    .then((response) => {
+      expect(response.status).to.eq(200)
+    })
+    cy.visit("https://app.dev.zendy.io/search");
+    cy.get('[type="search"]').type('test');
+    cy.get('.css-1n5o6em-root').contains('Search').click();
+    cy.wait(2000);
+    cy.get('[data-test="filters-container"]]').find('.css-jeurqa-filter-item-container').click();
     cy.get('[data-test="search-result-0"]').contains('Read').click();
 
   })
